74cdfee249bba277ad4182e733d7224d13f3a966
Kashaan-M <kashaanmahmood@gmail.com>
2022-12-30 12:16:47 +0500

    fixed comment

--- higher-order-procedures-1.js
+++ higher-order-procedures-1.js
@@ -5,7 +5,7 @@ const cubes = (x) => x * x * x;
 const sumSquares = (a, b) => {
   // returns sum of squares of numbers starting from a to b
   if (a > b) return 0;
-  return (a * a) + sumSquares(a + 1, b);
+  return a * a + sumSquares(a + 1, b);
 };
 /*
  * > sumSquares(3,5)
@@ -14,19 +14,19 @@ const sumSquares = (a, b) => {
 
 const sumCubes = (a, b) => {
   if (a > b) return 0;
-  return (a * a * a) + sumCubes(a + 1, b);
+  return a * a * a + sumCubes(a + 1, b);
 };
 /*
  * > sumCubes(3,5)
- * 216 
+ * 216
  */
 
 // higher order function which takes a function as a formal argument
 
-const sum = (fn,a,b) => {
-  if(a > b) return 0;
-  return (fn(a)) + sum(fn, a+1, b);
-}
+const sum = (fn, a, b) => {
+  if (a > b) return 0;
+  return fn(a) + sum(fn, a + 1, b);
+};
 
 /*
  * > sum(squares,3,5)
@@ -35,19 +35,20 @@ const sum = (fn,a,b) => {
  * 216
  */
 
- // checks for the words containing the check word in the given sentence and returns them 
+// checks for the words containing the check word in the given sentence and returns them
 function checkWord(sentence, check) {
   let arr = sentence.split(' ');
   let words = '';
-  arr.forEach(word => {
-    if(word.includes(check)) {
+  arr.forEach((word) => {
+    if (word.includes(check)) {
       words += ' ' + word;
     }
-  })
+  });
   words = words.trim();
   return words;
 }
 /*
  * > checkWord('A lot of garbage is on the streets someone needs to go and clean the streets','e')
  * "garbage the streets someone needs clean the streets"
-
+ *
+ */
