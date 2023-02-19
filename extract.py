f6aac1aa4060a29be38dda8cbedd984a795c3bb2
kashaan_mahmood <kashaanmahmood@gmail.com>
2023-02-19 14:37:21 +0500

    added a docx file

--- extract.py
+++ extract.py
@@ -2,16 +2,8 @@
     This module can be imported in other programs but the preffered way is to run it 
     as a Script from the command line like this:
     
-    $ python extract.py <name-of-word-document-file> <step>
+    $ python extract.py <name-of-word-document-file>
     
-    where <step> is approximately how many words the program will analyze in each loop. 
-    
-    For example, let's say there are 50 words in a word document.
-      If we run this program as a script on this word document and
-      provide <step> of 10 then the program will analyze 
-      10 words for each iteration
-      i.e. 0 - 10 , 10 - 20, 20 - 30, 30 - 40, 40 - 50
-
     The basic function of the program is to analyze the word document and 
     collect bold, italic and underlined words from it and then 
     after analyzing write these collected words at the end of the word document.
@@ -27,18 +19,13 @@
     """
 
 
-from math import ceil
 from docx import Document
 from docx.api import Document
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 # global variables
 total_words = 0
-start = 0
-end = 0
-step = 20
 wordsList = ""
-FROM = ""
 
 
 # calculate total words in docx
@@ -82,15 +69,6 @@ def analyze(docxFile):
     and return a `collect` object these selected words
     """
 
-    global start, end, step, total_words
-
-    total_words = get_total_words(docxFile)
-
-    if total_words - end > 0:
-        end = start + step
-    else:
-        end = total_words
-
     document = Document(docxFile)
 
     collect = [
@@ -103,44 +81,37 @@ def analyze(docxFile):
         {"biu": []},
     ]
 
-    # checks
-    checkRange = 0
-
     for p in document.paragraphs:
         for run in p.runs:
-            checkRange += len(run.text.split())
+            if run.bold and run.italic and run.underline:
+                filtered_text = remove_unwanted(run.text)
+                collect[6]["biu"].append(filtered_text)
 
-            if checkRange > start and checkRange < end:
-                if run.bold and run.italic and run.underline:
-                    filtered_text = remove_unwanted(run.text)
-                    collect[6]["biu"].append(filtered_text)
+            elif run.bold and run.italic:
+                filtered_text = remove_unwanted(run.text)
+                collect[3]["bi"].append(filtered_text)
 
-                elif run.bold and run.italic:
-                    filtered_text = remove_unwanted(run.text)
-                    collect[3]["bi"].append(filtered_text)
+            elif run.bold and run.underline:
+                filtered_text = remove_unwanted(run.text)
+                collect[4]["bu"].append(filtered_text)
 
-                elif run.bold and run.underline:
-                    filtered_text = remove_unwanted(run.text)
-                    collect[4]["bu"].append(filtered_text)
+            elif run.italic and run.underline:
+                filtered_text = remove_unwanted(run.text)
+                collect[5]["iu"].append(filtered_text)
 
-                elif run.italic and run.underline:
-                    filtered_text = remove_unwanted(run.text)
-                    collect[5]["iu"].append(filtered_text)
+            elif run.bold:
+                filtered_text = remove_unwanted(run.text)
+                collect[0]["b"].append(filtered_text)
 
-                elif run.bold:
-                    filtered_text = remove_unwanted(run.text)
-                    collect[0]["b"].append(filtered_text)
+            elif run.italic:
+                filtered_text = remove_unwanted(run.text)
+                collect[1]["i"].append(filtered_text)
 
-                elif run.italic:
-                    filtered_text = remove_unwanted(run.text)
-                    collect[1]["i"].append(filtered_text)
+            elif run.underline:
+                filtered_text = remove_unwanted(run.text)
+                collect[2]["u"].append(filtered_text)
 
-                elif run.underline:
-                    filtered_text = remove_unwanted(run.text)
-                    collect[2]["u"].append(filtered_text)
-
-            elif checkRange >= end:
-                return collect
+    return collect
 
 
 def write_data(docxFile, data):
@@ -148,7 +119,7 @@ def write_data(docxFile, data):
     and reads and appends the 'data' to end of docx file
     """
 
-    global start, end, wordsList, FROM
+    global wordsList
 
     document = Document(docxFile)
 
@@ -156,145 +127,66 @@ def write_data(docxFile, data):
         document.save(docxFile)
         return "saved"
 
-    # checks
-    checkEnd = 0
-    diff = 0
+    def add_words(key):
+        global wordsList
+        categories = {
+            "b": "\nBold Words:-",
+            "i": "\n\nItalicized Words:-",
+            "u": "\n\nUnderlined Words:-",
+            "bi": "\n\nBold & Italicized Words:-",
+            "bu": "\n\nBold & Underlined Words:-",
+            "biu": "\n\nBold & Italicized & Underlined Words:-",
+            "iu": "\n\nItalicized & Underlined Words:-",
+        }
+        for word in words[key]:
+            category = categories[key]
+            if len(wordsList) == 0 or category not in wordsList:
+                wordsList = wordsList + f"{category}\n{word}"
+            else:
+                wordsList = wordsList + f", {word}"
 
-    for p in document.paragraphs:
-        for run in p.runs:
-            checkEnd += len(run.text.split())
-            diff = end - checkEnd
+    title_p = document.add_paragraph(f"\n========== Extracted Words ==========\n")
 
-            if checkEnd == end or (diff < 1 and end > checkEnd):
+    title_p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-                def add_words(key):
-                    global wordsList
+    write_p = document.add_paragraph()
 
-                    for word in words[key]:
-                        if not word == "" and not word == " ":
-                            if key == "b":
-                                category = "Bold Words:-"
-                                if len(wordsList) == 0 or category not in wordsList:
-                                    wordsList = wordsList + f"{category}\n{word}"
-                                else:
-                                    wordsList = wordsList + f", {word}"
+    for words in data:
+        if words.__contains__("b") and words["b"]:
+            add_words("b")
 
-                            elif key == "i":
-                                category = "Italicized Words:-"
-                                if len(wordsList) == 0 or category not in wordsList:
-                                    wordsList = wordsList + f"\n\n{category}\n{word}"
-                                else:
-                                    wordsList = wordsList + f", {word}"
+        elif words.__contains__("u") and words["u"]:
+            add_words("u")
 
-                            elif key == "u":
-                                category = "Underlined  Words:-"
-                                if len(wordsList) == 0 or category not in wordsList:
-                                    wordsList = wordsList + f"\n\n{category}\n{word}"
-                                else:
-                                    wordsList = wordsList + f", {word}"
+        elif words.__contains__("bi") and words["bi"]:
+            add_words("bi")
 
-                            elif key == "bi":
-                                category = "Bold & Italicized Words:-"
-                                if len(wordsList) == 0 or category not in wordsList:
-                                    wordsList = wordsList + f"\n\n{category}\n{word}"
-                                else:
-                                    wordsList = wordsList + f", {word}"
+        elif words.__contains__("bu") and words["bu"]:
+            add_words("bu")
 
-                            elif key == "bu":
-                                category = "Bold & Underlined Words:-"
-                                if len(wordsList) == 0 or category not in wordsList:
-                                    wordsList = wordsList + f"\n\n{category}\n{word}"
-                                else:
-                                    wordsList = wordsList + f", {word}"
+        elif words.__contains__("iu") and words["iu"]:
+            add_words("iu")
 
-                            elif key == "iu":
-                                category = "Italicized & Underlined Words:-"
-                                if len(wordsList) == 0 or category not in wordsList:
-                                    wordsList = wordsList + f"\n\n{category}\n{word}"
-                                else:
-                                    wordsList = wordsList + f", {word}"
+        elif words.__contains__("biu") and words["biu"]:
+            add_words("biu")
 
-                            elif key == "biu":
-                                category = "Bold & Italicized & Underlined Words:-"
-                                if len(wordsList) == 0 or category not in wordsList:
-                                    wordsList = wordsList + f"\n\n{category}\n{word}"
-                                else:
-                                    wordsList = wordsList + f", {word}"
+    write_p.add_run(f"{wordsList}")
 
-                title_p = document.add_paragraph(
-                    f"\n========== Extracted Words - Range [{start} - {end}] ==========\n"
-                )
-                title_p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    ending_p = document.add_paragraph("\n===================\n")
+    ending_p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-                write_p = document.add_paragraph()
-
-                if not FROM:  # Start of file
-                    write_p.add_run("From:\n")
-                    write_p.add_run("START OF FILE \n\n")
-
-                else:
-                    write_p.add_run("From:\n")
-                    write_p.add_run(f"{FROM}\n\n")
-
-                write_p.add_run("To:\n")
-                write_p.add_run(f"'...{run.text}'\n\n")
-
-                FROM = f"'{run.text}...'"
-
-                for words in data:
-                    if words.__contains__("b") and words["b"]:
-                        add_words("b")
-
-                    elif words.__contains__("i") and words["i"]:
-                        add_words("i")
-
-                    elif words.__contains__("u") and words["u"]:
-                        add_words("u")
-
-                    elif words.__contains__("bi") and words["bi"]:
-                        add_words("bi")
-
-                    elif words.__contains__("bu") and words["bu"]:
-                        add_words("bu")
-
-                    elif words.__contains__("iu") and words["iu"]:
-                        add_words("iu")
-
-                    elif words.__contains__("biu") and words["biu"]:
-                        add_words("biu")
-
-                write_p.add_run(f"{wordsList}")
-
-                ending_p = document.add_paragraph("\n===================\n")
-                ending_p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-
-                save_document()
-                break
-
-            elif checkEnd > end:
-                return
-
-        # reset wordsList
-        wordsList = ""
+    save_document()
+    return
 
 
 # function calls inside main()
 
 
 def main():
-    global start, end, step, total_words
+    global total_words
 
-    if total_words == 0:
-        total_words = get_total_words(docx)
-
-    # number of times the program will read and write to docx
-    cycles = ceil(total_words / step)
-
-    for i in range(cycles):
-        data = analyze(docx)
-        write_data(docx, data)
-        start = end
-        end += step
+    data = analyze(docx)
+    write_data(docx, data)
 
 
 if __name__ == "__main__":
@@ -303,7 +195,7 @@ if __name__ == "__main__":
 
     # get docx file
     docx = argv[1]
-    step = int(argv[2])
+
     print(f"Started at {time.strftime('%X')}...")
     # calling main()
     main()
